20104df405aa87b7801b7bda74a4751fdae8146b
Jacob Kiggins <jkiggins@criticallink.com>
2018-09-21 13:28:36 -0400

    Changed color, added compton to i3/config, added pull.sh

---
+++ pull.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NAME_CHECK=$(git remote get-url origin | grep dot-configs.git)
+
+if [ -z $NAME_CHECK ]; then
+  echo "command must be run from within dot-configs"
+  exit 1
+fi;
+
+FILES=$(find -type f ! -path "./.git/*" ! -name "pull.sh")
+
+for f in $FILES; do
+  cp ~/${f} ${f}
+done
